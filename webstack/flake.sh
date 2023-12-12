89728a3309e7d6d813073d4c858990388b8d6ede
Ryan <ryancargan@gmail.com>
2023-12-12 11:38:05 +0530

    Add tweak to allow exec from another dir

--- webstack/flake.sh
+++ webstack/flake.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
+# Get the name of the current directory
+dir=$(basename "$PWD")
+
+# Navigate to the flake directory
+cd ../flakes/$dir
+
+# Activate the flake
 NIXPKGS_ALLOW_UNFREE=1 nix develop --impure
+
+# The script will end here, and you'll remain in the last directory navigated to by the script.
