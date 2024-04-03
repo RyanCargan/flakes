347caf1c67e080fd803d5abd804fc7ae7ab204a9
Ryan <ryancargan@gmail.com>
2024-04-03 15:56:11 +0530

    Add misc changes

--- cascadeur/flake.nix
+++ cascadeur/flake.nix
@@ -12,21 +12,30 @@
         libraries = with pkgs; [
           libxml2
           krb5
+          xorg.libxcb
+          qt5.qtbase
         ];
 
 
         packages = with pkgs; [ ] ++ libraries;
 
         devEnv = pkgs.mkShell {
-          buildInputs = packages; # Include the packages directly
+          buildInputs = with pkgs; [ ] ++ packages; # Include the packages directly
+          nativeBuildInputs = with pkgs; [
+            # qt5.wrapQtAppsHook
+          ];
 
-          shellHook =
+
+          shellHook = with pkgs;
             ''
               # Customize the shell prompt with a pretty blue color
               PS1="\[\033[1;34m\](nix-flake: compcode) \[\033[0m\]$PS1"
 
               # Export necessary environment variables
-              export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
+              export DISPLAY=:0
+              export LD_LIBRARY_PATH=${lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
+              export QT_DEBUG_PLUGINS=1
+              export QT_QPA_PLATFORM_PLUGIN_PATH=“${qt5.qtbase.bin}/lib/qt-${qt5.qtbase.version}/plugins”;
             '';
         };
       in
