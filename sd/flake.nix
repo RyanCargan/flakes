0ad5f88fb38fdc3453dff6b7b81697359fc45676
Ryan <ryancargan@gmail.com>
2024-04-29 08:12:09 +0530

    Add SD flake changes

--- sd/flake.nix
+++ sd/flake.nix
@@ -15,9 +15,6 @@
         pkgs = nixpkgs-unstable.legacyPackages.${system};
 
         # Overrides
-        tensorflowOverride = pkgs.python3Packages.tensorflow.override { cudaSupport = true; };
-
-        # SD
         torchOverride = pkgs.python3Packages.torch.override { cudaSupport = true; };
         torchSdeOverride = pkgs.python3Packages.torchsde.override { torch = torchOverride; };
         torchVisionOverride = pkgs.python3Packages.torchvision.override { torch = torchOverride; };
@@ -28,86 +25,9 @@
           torchaudio = torchAudioOverride;
         };
         safetensorsOverride = pkgs.python3Packages.safetensors.override { torch = torchOverride; };
-
-        opencv4Override = pkgs.python3Packages.opencv4.override { enableCuda = true; };
-        customCudnn = pkgs.cudaPackages.cudnn.overrideAttrs (oldAttrs: {
-          postFixup =
-            if pkgs.lib.versionAtLeast oldAttrs.version "8.0.5" then ''
-              patchelf $out/lib/libcudnn.so --add-needed libcudnn_cnn_infer.so
-              patchelf $out/lib/*.so --add-needed libcublas.so.11
-              patchelf $out/lib/*.so --add-needed libcublasLt.so.11
-            '' else "";
-        });
-        jaxlibOverride = pkgs.python3Packages.jaxlib.override {
-          cudaSupport = true;
-        };
-
-        # Define the jaxtyping package
-        jaxtyping = pkgs.python3Packages.buildPythonPackage rec {
-          pname = "jaxtyping";
-          version = "0.2.20";
-          format = "pyproject";
-
-          src = pkgs.python3Packages.fetchPypi {
-            inherit version;
-            inherit pname;
-            sha256 = "3CvRXgCy84rF3cAtOouwIC8XA62RAx6RS8d6do2duMU=";
-          };
-
-          buildInputs = [
-            pkgs.python3Packages.hatchling
-            pkgs.python3Packages.numpy
-            pkgs.python3Packages.typeguard
-            pkgs.python3Packages.typing-extensions
-          ];
-        };
-
-        # Define the equinox package
-        equinox = pkgs.python3Packages.buildPythonPackage rec {
-          pname = "equinox";
-          version = "0.10.1";
-          # format = "pyproject"; # Comment out for setup.py, setuptools may be another option.
-
-          src = pkgs.python3Packages.fetchPypi {
-            inherit version;
-            inherit pname;
-            sha256 = "468UMuOQmt08bl0MDmxRzedXTfrxFUz9n58DX921TfQ=";
-          };
-
-          buildInputs = [
-            pkgs.python3Packages.hatchling
-            pkgs.python3Packages.jax
-            jaxtyping
-            pkgs.python3Packages.typing-extensions
-            pkgs.python3Packages.typeguard
-          ];
-
-          doCheck = false; # Disable the check/test phase
-        };
-
-        # Define the Gooey package
-        Gooey = pkgs.python3Packages.buildPythonPackage rec {
-          pname = "Gooey";
-          version = "1.0.8.1";
-
-          src = pkgs.python3Packages.fetchPypi {
-            inherit version;
-            inherit pname;
-            sha256 = "CNa/U09NUNUNr7pc/Gjc8xpunu7xOpTL4+oXxORcRnE=";
-          };
-
-          buildInputs = [
-            pkgs.python3Packages.wxPython_4_2
-            pkgs.python3Packages.pygtrie
-            pkgs.python3Packages.colored
-            pkgs.python3Packages.psutil
-          ];
-
-          doCheck = false; # Disable the check/test phase, since it seems to require an X display
-        };
+        korniaOverride = pkgs.python3Packages.kornia.override { torch = torchOverride; };
 
         python = pkgs.python3.withPackages (ps: with ps; [
-          # SD
           torchOverride
           torchSdeOverride
           torchVisionOverride
@@ -121,81 +41,22 @@
           scipy
           tqdm
           psutil
-
-          # General dependencies
-          numpy
-          typing-extensions
-          jax
-          optax
-          jaxlibOverride
-          fastapi
-          psycopg2
-          pyautogui
-          opencv4Override
-          tesserocr
-          pillow
-
-          # Gooey and its dependencies
-          Gooey
-          psutil
-          colored
-          pygtrie
-          wxPython_4_2
-
-          # GUI libraries
-          pygobject3
-          pyside6
-          pynput
-
-          # Data loading utilities for JAX
-          # torchOverride
-          # torchVisionOverride
-          # torchvision
-
-          # Machine learning
-          tensorflowOverride
-          # transformers
-          sentencepiece
-          pandas
-
-          # Visualization
-          matplotlib
-
-          # Simulation
-          pyglet
-          pygame
-          pybullet
-          pyopengl
-
-          # Equinox and its dependencies
-          equinox
-          jaxtyping
-          typeguard
-
-          # PDF manipulation
-          pdf2image
-
-          # DOCX manipulation
-          python-docx
-
-          # Anki
-          genanki
+          korniaOverride
         ]);
 
         libraries = with pkgs; [
           cudaPackages.cudatoolkit # pkg/lib combo
-          customCudnn
-          cudaPackages.libcublas
-          cudaPackages.libcufft
-          cudaPackages.libcurand
-          cudaPackages.libcusolver
-          cudaPackages.libcusparse
+          # cudaPackages.cudnn
+          # cudaPackages.libcublas
+          # cudaPackages.libcufft
+          # cudaPackages.libcurand
+          # cudaPackages.libcusolver
+          # cudaPackages.libcusparse
           # linuxHeaders
         ];
 
         packages = with pkgs; [
           python
-          scrot
         ] ++ libraries;
 
         devEnv = pkgs.mkShell {
