c9a9fd9c12381a701145cc268b3ac99b0d501487
Ryan <ryancargan@gmail.com>
2024-05-03 19:31:49 +0530

    Add build flags

--- sd/build.sh
+++ sd/build.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-NIXPKGS_ALLOW_UNFREE=1 nix build .#myEnv --impure -o sd-flake-gcroot-result
+# Set environment variables to control the build process
+export NIXPKGS_ALLOW_UNFREE=1    # Allow building packages with non-free licenses
+export NIX_BUILD_CORES=4         # Limit the number of cores per build
+export MAKEFLAGS="-j4"           # Limit the number of jobs for make
+
+# Run the Nix build with additional options for job control
+nix build .#myEnv --impure -o sd-flake-gcroot-result --max-jobs 4
